94f40d00c0ec623641578e37bfdfa9826ebf166f
sagieppel <sagieppel@gmail.com>
2017-07-27 01:48:42 +0300

    Update Train.py

--- Train.py
+++ Train.py
@@ -120,6 +120,6 @@ def main(argv=None):
         if itr%200==0 :
             print("Saving Model")
             saver.save(sess, logs_dir + "model.ckpt", itr)# save trained model
-print("Finished Running")
+
 if __name__ == "__main__":
     tf.app.run()
